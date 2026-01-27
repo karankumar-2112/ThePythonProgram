532c5433b236de9a1e4e832d6881ebb271981db3
Karan <mamtarajkumar1212@gmail.com>
2026-01-27 13:13:00 -0800

    Updated

---
+++ Excercise of Operators.py
@@ -0,0 +1,95 @@
+"""
+# (1)
+a = 5
+b = 10
+print(a <b < 20) # True
+
+# (2)
+x = True
+y = False
+print(x + y * x) # 1
+
+# (3)
+print(4**0**2) # 1
+
+
+# (4)
+a = 12
+b = 5
+print(a^b) # 9
+
+
+# (5)
+print((3 and 0)or(0 and 3)) # 0
+
+
+# (6)
+print(256 is 256) # True
+print(257 is 257) # True
+
+# (7)
+a = 7
+print(~a + 1) # -7
+
+# (8)
+a = True
+b = False
+print((a or b) and not (a and b)) # True
+
+
+# (9)
+print(10 > 5 == True) # False
+
+
+# (10)
+print(2 + 3 * 4 ** 2 // 8 % 3) # 2
+
+
+# (11)
+print(1 << 2 + 1) # 8
+
+# (12)
+a = 3
+b = 2
+a *= b + 1
+print(a) #9
+
+
+#(13)
+print(3 < 5 > 2 == 2) # True
+
+
+#(14)
+print(10 // 3 * 3 + 1 % 3) # 10
+
+
+
+# (15)
+x = 10
+y = 20
+print(x & y | x ^ y) # 30
+
+
+# (16)
+a = True
+b = False
+print(a & b or a ^ b) # True
+
+
+# (17)
+x = 8
+print(x >> 1 << 2) # 16
+
+
+# (18)
+print(5 + True * False + (not False)) # 6
+
+
+# (19)
+print((not 0) * (False or 1)) # 1
+
+# (20)
+print(4 + 3 * 2 ** 2 // 2 - 1) # 9
+
+
+"""
